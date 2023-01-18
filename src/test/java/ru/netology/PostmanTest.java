223fd6a9bd03cd2565d3937e327069d444415c6e
Alexandra Chernova <chernova.aleksandra28@gmail.com>
2023-01-18 21:33:05 +0300

    add test

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+
+
+class PostmanTest {
+    @Test
+    void shouldTestPostman() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency"));
+
+    }
+
+}
